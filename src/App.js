7b9380e49d450c182e23d573d46a041ad7052773
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-05-29 21:03:50 +0200

    create inital board layout with empty cells

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import styles from './App.module.scss'
+import styles from './App.module.scss';
+import { TetrisBoard } from './components/tetris-board';
+
+const rows = 20;
+const columns = 10;
+
+const board = new Array(rows).fill(0).map(() => new Array(columns).fill(0));
 
 const App = () => {
+
   return (
     <div className={styles.App}>
-      <main className={styles.main}>
-        main
-      </main>
+      <TetrisBoard board={board} />
     </div>
   );
 }
---
+++ src/components/tetris-board/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './board.scss';
+
+const TetrisBoard = ({ board }) => {
+  const rows = [];
+
+  board.forEach((row, index) => {
+    const columns = row.map((column, index) => <div className={`column block-${column}`} />);
+    rows.push(<div className="tetris-board__row" key={index}>{columns}</div>);
+  })
+
+  return (
+    <div className="tetris-board">
+      <div className="tetris-board__board">
+        {rows}
+      </div>
+    </div>
+  )
+};
+
+export { TetrisBoard };
